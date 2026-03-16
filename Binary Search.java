eb38d0202e92a7c927db318ba07105e606107e8f
LIKHITH  HP <hplikhith690@gmail.com>
2026-03-16 20:02:38 +0530

    Add files via upload

---
+++ Binary Search.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,30,40,50};
+        int key = 30;
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == key) {
+                System.out.println("Found at " + mid);
+                break;
+            }
+            else if(arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+}
